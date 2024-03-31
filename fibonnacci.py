d39fe28a9144bc6eb09d54fd1ab0e4ad016df0b4
firstOzu <106449420+firstOzu@users.noreply.github.com>
2024-03-31 21:25:45 +0200

    Add files via upload

---
+++ fibonnacci.py
@@ -0,0 +1,25 @@
+#/usr/bin/python3
+
+#  Code Alpha Python Intership, Task 1 #
+#~ Implementation of fibonacci serie using recursion. ~#
+#~ Made by Byylle ODOALEYKOUN, aka Ozu. ~#
+
+def menu():
+    print("""Welcome to my fibonacci series printing program. 
+             Just enter the number of term you want and you'll have it. """)
+
+def fibonacci(numbers):
+    fibo = [0,1]
+    if (numbers==0):
+        return []
+    elif (numbers in (1,2)):
+        return fibo
+    else: 
+        for i in range(0,numbers-2):
+            fibo.append(fibo[-1]+fibo[-2])
+    return fibo
+
+menu()
+
+print(fibonacci(int(input("How many terms do you want to print? : "))))
+
